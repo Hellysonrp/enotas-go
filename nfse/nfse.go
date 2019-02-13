5d9cfdb31e42f9cc33c06f0ef946a54d8ba3d040
Breno Martinusso <martinusso@gmail.com>
2019-02-13 10:15:05 -0200

    Add return method cancel (#2)

--- nfse/nfse.go
+++ nfse/nfse.go
@@ -29,13 +29,19 @@ func (n NFSe) Habilitar(empresaID string) error {
 	return nil
 }
 
-func (n NFSe) Cancelar(empresaID, id string) error {
+func (n NFSe) Cancelar(empresaID, id string) (string, error) {
 	url := fmt.Sprintf("%s/empresas/%s/nfes/%s", config.Endpoint, empresaID, id)
 	response := n.client.Delete(url)
 	if response.Error != nil {
-		return response.Error
+		return "", response.Error
 	}
-	return nil
+	var ret struct {
+		ID string `json:"nfeId"`
+	}
+	if err := json.Unmarshal(response.Body, &ret); err != nil {
+		return "", errors.New("Erro no retorno do cancelamento da Nota Fiscal.")
+	}
+	return ret.ID, nil
 }
 
 func (n NFSe) Consultar(empresaID, id string) (entity.NFSe, error) {
