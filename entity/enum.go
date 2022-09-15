711f1d583c1db782eae5cdc8cc3dfd34a88f448a
Hellyson Rodrigo Parteka <hellysonrp@gmail.com>
2022-09-15 00:23:17 -0300

    Update enum.go

--- entity/enum.go
+++ entity/enum.go
@@ -18,20 +18,22 @@ const (
 	TipoOperacao_Entrada TipoOperacao = "Entrada"
 
 	FinalidadeNFe_Normal              FinalidadeNFe = "Normal"              // 1
-	FinalidadeNFe_Complementar        FinalidadeNFe = "Complementar"        // 2 - não tenho certeza de que a string é essa mesmo; não possui documentação na enotas
-	FinalidadeNFe_Ajuste              FinalidadeNFe = "Ajuste"              // 3 - não tenho certeza de que a string é essa mesmo; não possui documentação na enotas
+	FinalidadeNFe_Complementar        FinalidadeNFe = "Complementar"        // 2
+	FinalidadeNFe_Ajuste              FinalidadeNFe = "Ajuste"              // 3
 	FinalidadeNFe_DevolucaoMercadoria FinalidadeNFe = "DevolucaoMercadoria" // 4
 
-	IndicadorPresencaConsumidor_NaoSeAplica          IndicadorPresencaConsumidor = "NaoSeAplica"             // 0
-	IndicadorPresencaConsumidor_OperacaoPresencial   IndicadorPresencaConsumidor = "OperacaoPresencial"      // 1
-	IndicadorPresencaConsumidor_OperacaoPelaInternet IndicadorPresencaConsumidor = "OperacaoPelaInternet"    // 2
-	IndicadorPresencaConsumidor_Teleatendimento      IndicadorPresencaConsumidor = "OperacaoTeleAtendimento" // 3
-	IndicadorPresencaConsumidor_Outros               IndicadorPresencaConsumidor = "Outros"                  // 9
-	// TODO outros indicadores de presença
+	IndicadorPresencaConsumidor_NaoSeAplica                             IndicadorPresencaConsumidor = "NaoSeAplica"                             // 0
+	IndicadorPresencaConsumidor_OperacaoPresencial                      IndicadorPresencaConsumidor = "OperacaoPresencial"                      // 1
+	IndicadorPresencaConsumidor_OperacaoPelaInternet                    IndicadorPresencaConsumidor = "OperacaoPelaInternet"                    // 2
+	IndicadorPresencaConsumidor_Teleatendimento                         IndicadorPresencaConsumidor = "OperacaoTeleAtendimento"                 // 3
+	IndicadorPresencaConsumidor_EntregaADomicilio                       IndicadorPresencaConsumidor = "EntregaADomicilio"                       // 4
+	IndicadorPresencaConsumidor_OperacaoPresencialForaDoEstabelecimento IndicadorPresencaConsumidor = "OperacaoPresencialForaDoEstabelecimento" // 5
+	IndicadorPresencaConsumidor_Outros                                  IndicadorPresencaConsumidor = "Outros"                                  // 9
 
-	ModalidadeFrete_PorContaDoEmitente     ModalidadeFrete = "PorContaDoEmitente"     // 0
-	ModalidadeFrete_PorContaDoDestinatario ModalidadeFrete = "PorContaDoDestinatario" // 1
-	ModalidadeFrete_PorContaDeTerceiros    ModalidadeFrete = "PorContaDeTerceiros"    // 2
-	ModalidadeFrete_SemFrete               ModalidadeFrete = "SemFrete"               // 9
-	// TODO 3 e 4
+	ModalidadeFrete_PorContaDoEmitente                      ModalidadeFrete = "ContratacaoPorContaDoRemetente"          // 0
+	ModalidadeFrete_PorContaDoDestinatario                  ModalidadeFrete = "ContratacaoPorContaDoDestinatario"       // 1
+	ModalidadeFrete_PorContaDeTerceiros                     ModalidadeFrete = "ContratacaoPorContaDeTerceiros"          // 2
+	ModalidadeFrete_TransporteProprioPorContaDoRemetente    ModalidadeFrete = "TransporteProprioPorContaDoRemetente"    // 3
+	ModalidadeFrete_TransporteProprioPorContaDoDestinatario ModalidadeFrete = "TransporteProprioPorContaDoDestinatario" // 4
+	ModalidadeFrete_SemFrete                                ModalidadeFrete = "SemOcorrenciaDeTransporte"               // 9
 )
