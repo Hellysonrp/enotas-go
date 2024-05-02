0eb9c6a2c7bbbb0d3a6b3c564fc88d1891b71d9d
Otto Millrath <ottomillrath@gmail.com>
2024-05-02 11:39:23 -0300

    fix: number or string

--- entity/servico.go
+++ entity/servico.go
@@ -1,19 +1,21 @@
 package entity
 
+import "encoding/json"
+
 // Servico representa o objeto contendo os dados do serviço prestado
 type Servico struct {
-	CNAE                      string   `json:"cnae"`
-	CodigoServicoMunicipio    string   `json:"codigoServicoMunicipio"`
-	Descricao                 string   `json:"descricao"`
-	AliquotaISS               *float64 `json:"aliquotaIss,omitempty"`
-	IssRetidoFonte            *bool    `json:"issRetidoFonte,omitempty"`
-	ValorPIS                  *float64 `json:"valorPis,omitempty"`
-	ValorCOFINS               *float64 `json:"valorCofins,omitempty"`
-	ValorCSLL                 *float64 `json:"valorCsll,omitempty"`
-	ValorINSS                 *float64 `json:"valorInss,omitempty"`
-	ValorIR                   *float64 `json:"valorIr,omitempty"`
-	ValorISS                  *float64 `json:"valorIss,omitempty"`
-	MunicipioPrestacaoServico string   `json:"municipioPrestacaoServico,omitempty"`
+	CNAE                      string      `json:"cnae"`
+	CodigoServicoMunicipio    string      `json:"codigoServicoMunicipio"`
+	Descricao                 string      `json:"descricao"`
+	AliquotaISS               *float64    `json:"aliquotaIss,omitempty"`
+	IssRetidoFonte            *bool       `json:"issRetidoFonte,omitempty"`
+	ValorPIS                  *float64    `json:"valorPis,omitempty"`
+	ValorCOFINS               *float64    `json:"valorCofins,omitempty"`
+	ValorCSLL                 *float64    `json:"valorCsll,omitempty"`
+	ValorINSS                 *float64    `json:"valorInss,omitempty"`
+	ValorIR                   *float64    `json:"valorIr,omitempty"`
+	ValorISS                  *float64    `json:"valorIss,omitempty"`
+	MunicipioPrestacaoServico json.Number `json:"municipioPrestacaoServico,omitempty"`
 }
 
 // NewService cria um novo serviço
